254e3a145f40da4daa7a4e6e9c1ed0cf356d5b2d
whyrusleeping <why@ipfs.io>
2021-04-08 08:21:33 -0700

    add a basic command to check which multisigs a given key is a signer of

--- cmd/lotus-shed/misc.go
+++ cmd/lotus-shed/misc.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/multisig"
+	"github.com/filecoin-project/lotus/chain/types"
+	lcli "github.com/filecoin-project/lotus/cli"
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"github.com/urfave/cli/v2"
 )
 
@@ -14,6 +23,7 @@ var miscCmd = &cli.Command{
 	Flags: []cli.Flag{},
 	Subcommands: []*cli.Command{
 		dealStateMappingCmd,
+		msigOwnershipCheckCmd,
 	},
 }
 
@@ -34,6 +44,74 @@ var dealStateMappingCmd = &cli.Command{
 			return fmt.Errorf("no such deal state %d", num)
 		}
 		fmt.Println(ststr)
+		return nil
+	},
+}
+
+var msigOwnershipCheckCmd = &cli.Command{
+	Name: "ownership-check",
+	Action: func(cctx *cli.Context) error {
+		if !cctx.Args().Present() {
+			return cli.ShowCommandHelp(cctx, cctx.Command.Name)
+		}
+
+		api, closer, err := lcli.GetFullNodeAPI(cctx)
+		if err != nil {
+			return err
+		}
+
+		defer closer()
+		ctx := lcli.ReqContext(cctx)
+
+		addr, err := address.NewFromString(cctx.Args().First())
+		if err != nil {
+			return err
+		}
+
+		ida, err := api.StateLookupID(ctx, addr, types.EmptyTSK)
+		if err != nil {
+			return err
+		}
+
+		addrs, err := api.StateListActors(ctx, types.EmptyTSK)
+		if err != nil {
+			return err
+		}
+
+		bs := blockstore.NewAPIBlockstore(api)
+		cst := cbor.NewCborStore(bs)
+		store := adt.WrapStore(ctx, cst)
+
+		total := abi.NewTokenAmount(0)
+		for _, a := range addrs {
+
+			act, err := api.StateGetActor(ctx, a, types.EmptyTSK)
+			if err != nil {
+				return err
+			}
+
+			if builtin.IsMultisigActor(act.Code) {
+				mst, err := multisig.Load(store, act)
+				if err != nil {
+					return err
+				}
+
+				sigs, err := mst.Signers()
+				if err != nil {
+					return err
+				}
+
+				for _, signer := range sigs {
+					if signer == ida {
+						fmt.Printf("%s\t%s\n", a, types.FIL(act.Balance))
+						total = types.BigAdd(total, act.Balance)
+						break
+					}
+				}
+
+			}
+		}
+		fmt.Printf("total: %s\n", types.FIL(total))
 		return nil
 	},
 }
